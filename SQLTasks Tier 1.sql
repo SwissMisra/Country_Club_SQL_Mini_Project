4ed2358c00b6b03670f7d2893b79ecc8e87892be
Siddhartha Misra <misra.siddhartha@gmail.com>
2020-06-20 22:29:35 -0400

    Completed project

--- SQLTasks Tier 1.sql
+++ SQLTasks Tier 1.sql
@@ -1,11 +1,10 @@
 /* Welcome to the SQL mini project. You will carry out this project partly in
 the PHPMyAdmin interface, and partly in Jupyter via a Python connection.
 
-This is Tier 2 of the case study, which means that there'll be less guidance for you about how to setup
-your local SQLite connection in PART 2 of the case study. This will make the case study more challenging for you: 
-you might need to do some digging, aand revise the Working with Relational Databases in Python chapter in the previous resource.
+This is Tier 1 of the case study, which means that there'll be more guidance for you about how to 
+setup your local SQLite connection in PART 2 of the case study. 
 
-Otherwise, the questions in the case study are exactly the same as with Tier 1. 
+The questions in the case study are exactly the same as with Tier 2. 
 
 PART 1: PHPMyAdmin
 You will complete questions 1-9 below in the PHPMyAdmin interface. 
@@ -35,55 +34,34 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
 
-SELECT *
-FROM `Facilities`
-WHERE `membercost` >0
 
 /* Q2: How many facilities do not charge a fee to members? */
-SELECT COUNT( `facid` )
-FROM `Facilities`
-WHERE `membercost` = 0
+
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
-SELECT `facid` , `name` , `membercost` , `monthlymaintenance`
-FROM `Facilities`
-WHERE `membercost` * .2 < `monthlymaintenance`
+
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
-SELECT *
-FROM `Facilities`
-WHERE `facid`
-IN ( 1, 5 )
+
 
 /* Q5: Produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
-SELECT `name` , `monthlymaintenance` ,
-CASE WHEN `monthlymaintenance` > 100
-THEN 'expensive'
-WHEN `monthlymaintenance` < 100
-THEN 'cheap'
-END AS type
-FROM `Facilities`
+
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
-SELECT `firstname` , `surname` , MAX( `joindate` )
-FROM `Members`
-WHERE `firstname` != 'GUEST'
-
 
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
-
 
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
@@ -98,10 +76,20 @@ Order by descending cost, and do not use any subqueries. */
 
 
 /* PART 2: SQLite
+/* We now want you to jump over to a local instance of the database on your machine. 
 
-Export the country club data from PHPMyAdmin, and connect to a local SQLite instance from Jupyter notebook 
-for the following questions.  
+Copy and paste the LocalSQLConnection.py script into an empty Jupyter notebook, and run it. 
 
+Make sure that the SQLFiles folder containing thes files is in your working directory, and
+that you haven't changed the name of the .db file from 'sqlite\db\pythonsqlite'.
+
+You should see the output from the initial query 'SELECT * FROM FACILITIES'.
+
+Complete the remaining tasks in the Jupyter interface. If you struggle, feel free to go back
+to the PHPMyAdmin interface as and when you need to. 
+
+You'll need to paste your query into value of the 'query1' variable and run the code block again to get an output.
+ 
 QUESTIONS:
 /* Q10: Produce a list of facilities with a total revenue less than 1000.
 The output of facility name and total revenue, sorted by revenue. Remember
